712e50c7b591fe2ece14cc38504e8034c6a06fff
lerakry <lerakrya8@gmail.com>
2020-10-28 00:41:36 +0300

    Исправления в CMake

--- DZ2/cmake-build-debug/CTestTestfile.cmake
+++ DZ2/cmake-build-debug/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(testing "TEST" "COMMAND" "tests")
-set_tests_properties(testing PROPERTIES  _BACKTRACE_TRIPLES "/home/lerakry/github_tp_c_cpp_h1/tp_c-cpp_homework1/DZ2/CMakeLists.txt;39;add_test;/home/lerakry/github_tp_c_cpp_h1/tp_c-cpp_homework1/DZ2/CMakeLists.txt;0;")
+set_tests_properties(testing PROPERTIES  _BACKTRACE_TRIPLES "/home/lerakry/github_tp_c_cpp_h1/tp_c-cpp_homework1/DZ2/CMakeLists.txt;42;add_test;/home/lerakry/github_tp_c_cpp_h1/tp_c-cpp_homework1/DZ2/CMakeLists.txt;0;")
