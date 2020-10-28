f6e80a55d8f95b20ef24aeb4249f0923b3a0c33f
lerakry <lerakrya8@gmail.com>
2020-10-28 23:01:13 +0300

    Добавление .travis

---
+++ DZ2/cmake-build-debug-coverage/CMakeFiles/DZ2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for DZ2.
+# This may be replaced when dependencies are built.
---
+++ DZ2/cmake-build-debug/CMakeFiles/compute_parallal.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/lerakry/github_tp_c_cpp_h1/tp_c-cpp_homework1/DZ2/project/src/functions.c" "/home/lerakry/github_tp_c_cpp_h1/tp_c-cpp_homework1/DZ2/cmake-build-debug/CMakeFiles/compute_parallal.dir/project/src/functions.c.o"
+  "/home/lerakry/github_tp_c_cpp_h1/tp_c-cpp_homework1/DZ2/project/src/functions_parall.c" "/home/lerakry/github_tp_c_cpp_h1/tp_c-cpp_homework1/DZ2/cmake-build-debug/CMakeFiles/compute_parallal.dir/project/src/functions_parall.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "compute_parallal_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../project/headers"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
